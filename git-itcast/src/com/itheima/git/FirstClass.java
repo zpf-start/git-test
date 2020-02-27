ea5801ee126f3158e0f5b6a64295e70d693f7aa0
zhoupanfeng <1541086084@qq.com>
2020-02-27 23:38:19 +0800

    修改FirstClass

--- git-itcast/src/com/itheima/git/FirstClass.java
+++ git-itcast/src/com/itheima/git/FirstClass.java
@@ -8,11 +8,14 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+
+        //hello test
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
